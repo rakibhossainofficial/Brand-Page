f777366947fdd665a3a1bfd67aecacde469a31ce
rakibhossainofficial <rakib20@gmail.com>
2024-09-23 23:06:11 +0600

    2nd commit

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,31 +1,47 @@
 import React from 'react'
 import "./Navbar.css"
+import { useState } from 'react';
 
 const Navbar = () => {
+    const [menuOpen, setMenuOpen] = useState(false);
+
+    const toggleMenu = () => {
+      setMenuOpen(!menuOpen);
+    };
+  
   return (
     <>
-        <nav>
-            <div className="logo">
-            <img src="/images/brand_logo.png" alt="logo" />
-            </div>
-            <ul>
-            <li>
-                <a href="#">Menu</a>
-            </li>
-            <li>
-                <a href="#">Location</a>
-            </li>
-            <li>
-                <a href="#">About</a>
-            </li>
-            <li>
-                <a href="#">Contact</a>
-            </li>
-            </ul>
-            <button>Login</button>
-        </nav>
+      <nav>
+        <div className="logo">
+          <img src="/images/brand_logo.png" alt="logo" />
+        </div>
+        <ul className={menuOpen ? 'nav-links open' : 'nav-links'}>
+          <li>
+            <a href="#">Menu</a>
+          </li>
+          <li>
+            <a href="#">Location</a>
+          </li>
+          <li>
+            <a href="#">About</a>
+          </li>
+          <li>
+            <a href="#">Contact</a>
+          </li>
+        </ul>
+        <button>Login</button>
+
+        {/* Hamburger menu */}
+      <div className={menuOpen ? 'hamburger open' : 'hamburger'} onClick={toggleMenu}>
+        <span className="bar"></span>
+        <span className="bar"></span>
+        <span className="bar"></span>
+      </div>
+       
+
+      </nav>
     </>
-  )
+  );
 }
 
 export default Navbar
